25c6c1fbebcc5da677923907af580e0126c8bd4b
Brendon J. Brewer <brendon.brewer@gmail.com>
2014-10-29 12:49:11 +1300

    Fixing up plot

--- Code/transit_data.py
+++ Code/transit_data.py
@@ -2,6 +2,10 @@ import numpy as np
 import numpy.random as rng
 import matplotlib.pyplot as plt
 
+# Plotting stuff
+plt.rc("font", size=16, family="serif", serif="Computer Sans")
+plt.rc("text", usetex=True)
+
 # Seed random number generator
 rng.seed(0)
 
@@ -15,7 +19,7 @@ y = 10. + np.zeros(t.shape)
 y[np.abs(t - 3.5) < 1.] = 5.
 
 # Plot the signal
-plt.plot(t, y, 'r-', alpha=0.2)
+plt.plot(t, y, 'r-', alpha=0.5)
 
 # Add noise
 sig = 1.
@@ -27,7 +31,12 @@ data[:,0], data[:,1], data[:,2] = t, y, sig
 np.savetxt('transit_data.txt', data)
 
 # Plot the noisy data
-plt.errorbar(t, y, yerr=data[:,2], fmt='b.', alpha=0.2)
+plt.errorbar(t, y, yerr=data[:,2], fmt='b.', alpha=0.5)
 plt.axis([-1., 11., 0., 15.])
+plt.xlabel('Time')
+plt.ylabel('Magnitude')
+
+# Save the figure
+plt.savefig('transit_data.pdf', bbox_inches='tight')
 plt.show()
 
