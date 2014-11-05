07eba678a30ad35e70655b34c26f0545ce916cc5
Brendon Brewer <brendon.brewer@gmail.com>
2014-11-05 13:07:34 +0000

    Semi-working

--- Code/asteroseismology_model.py
+++ Code/asteroseismology_model.py
@@ -3,27 +3,38 @@ import numpy.random as rng
 import copy
 
 # How many parameters are there?
-num_params = 3
+num_params = 32
 
 # Load the data
 data = np.loadtxt('asteroseismology_data.txt')
 
 # Some properties of the data
-nu_min, nu_max = data[:,0].min(), data[:,0].max()
-nu_range = nu_max - nu_min
+x_min, x_max = data[:,0].min(), data[:,0].max()
+x_range = x_max - x_min
 N = data.shape[0] # Number of data points
 
 # Some idea of how big the Metropolis proposals should be
-jump_sizes = np.array([1., 1., 1.])
+jump_sizes = np.ones(num_params)
+jump_sizes[0] = 10.
+jump_sizes[1] = np.log(1E6)
 
 def from_prior():
   """
   A function to generate parameter values from the prior.
   Returns a numpy array of parameter values.
   """
-  u = rng.rand(3)
+  params = rng.rand(num_params)
 
-  return u
+  # Number of components (round down to get integer)
+  params[0] = 10.*params[0]
+
+  # log of background
+  params[1] = np.log(1E-3) + np.log(1E6)*rng.rand()
+
+  # The rest are for the amplitudes, centers, and widths of the peaks,
+  # and have U(0, 1) priors (they'll be transformed before being used)
+
+  return params
 
 def log_prior(params):
   """
@@ -31,7 +42,13 @@ def log_prior(params):
   """
 
   # Minus infinity, if out of bounds
-  if np.any(params < 0) or np.any(params > 1):
+  if params[0] < 0. or params[0] > 10.:
+    return -np.Inf
+
+  if params[1] < np.log(1E-3) or params[1] > np.log(1E3):
+    return -np.Inf
+
+  if np.any(params[2:] < 0) or np.any(params[2:] > 1):
     return -np.Inf
 
   return 0.
@@ -41,16 +58,22 @@ def log_likelihood(params):
   Evaluate the (log of the) likelihood function
   """
   # Rename the parameters
-  # Also transform away from U(0, 1)
-  A = -10.*np.log(1. - params[0])
-  nu_c = nu_min + nu_range*params[1]
-  width = np.exp(np.log(1E-3*nu_range) + np.log(1E3)*nu_range)
+  num_peaks = int(params[0])
+  B = np.exp(params[1])
 
-  # First calculate the expected signal
-  mu = 10. + np.zeros(N)
+  # Calculate the expected/model signal
+  mu = B + np.zeros(N)
 
-  # Add the Lorentzians
-  mu += A/(np.pi)/(1. + ((data[:,0] - nu_c)/width)**2)
+  # Add the peaks
+  k = 2
+  for i in range(0, num_peaks):
+    # Get the parameters
+    A = -10.*np.log(params[k])
+    xc = x_min + x_range*params[k+1]
+    width = np.exp(np.log(1E-3*x_range) + np.log(1E3)*params[k+2])
+
+    # Add the Lorentzian peak
+    mu += A/(1. + ((data[:,0] - xc)/width)**2)
 
   # Exponential distribution
   return np.sum(-np.log(mu) - data[:,1]/mu)
@@ -67,5 +90,4 @@ def proposal(params):
   which = rng.randint(num_params)
   new[which] += jump_sizes[which]*10.**(1.5 - 6.*rng.rand())*rng.randn()
   return new
-
 
