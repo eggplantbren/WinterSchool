35e9b5f7e52305f96ac6e8b9aeb6ed07b6d4433c
Brendon Brewer <brendon.brewer@gmail.com>
2015-04-27 15:58:42 +1200

    Shaded area, obtained points

--- Document/nested.py
+++ Document/nested.py
@@ -7,11 +7,25 @@ rc("text", usetex=True)
 Make nested sampling figures
 """
 
-X = linspace(0, 1, 1001)
+seed(123)
+
+X = linspace(0.001, 1, 1001)
 L = exp(sqrt(-log(X)))
 
-plot(X, L, 'b')
+plot(X, L, 'k', linewidth=2, label='True Curve')
 xlabel('$X$')
-ylabel('$L$')
+ylabel('Likelihood')
+
+# Colour the area below the curve
+ax = gca()
+ax.fill_between(X, L, alpha=0.2)
+
+# Add some random points
+X = exp(-5 + 5*rand(10))
+L = exp(sqrt(-log(X)))
+plot(X, L, 'ko', markersize=10, label='Points Obtained')
+legend(numpoints=1)
+
+savefig('nested1.pdf', bbox_inches='tight')
 show()
 
